316686bd1771708dd2c3f4943c9062a5b3659ef9
Tristan Debroise <tristandebroise@gmail.com>
2019-05-15 17:43:07 +0200

    env example

---
+++ src/environement/environement-empty.js
@@ -0,0 +1,14 @@
+/* PUT HERE ALL THE ENV VARIABLES NEEDED */
+
+export const FIREBASE_CONFIG = {
+  // config from firebase
+}
+
+// Facebook App Id
+export const FB_APP_ID = ''
+
+// Google Client Id
+export const GOOGLE_CLIENT_ID = ""
+// put this client id in the firebase console
+// create client android https://console.cloud.google.com/apis/credentials?
+// see https://blog.expo.io/google-sign-in-with-react-native-and-expo-9cac6c392f0e
