c36a1472dfd6e000f243b71bbabf68b78914a00b
Tristan Debroise <tristandebroise@gmail.com>
2019-05-15 17:39:28 +0200

    removed api keys

--- src/components/SocialLogin/Facebook.js
+++ src/components/SocialLogin/Facebook.js
@@ -2,18 +2,16 @@ import React from 'react'
 import { Button, View, Text } from 'react-native'
 import { Facebook } from 'expo';
 import * as firebase from 'firebase'
-
+import { FB_APP_ID } from 'src/environement'
 class FacebookButton extends React.Component {
   onClickLogin = async () => {
     const { type, token } = await Facebook.logInWithReadPermissionsAsync(
-      '2077132879061960', // Your app id from Facebook dev
+      FB_APP_ID, // Your app id from Facebook dev
       { permissions: ['public_profile'] }
     );
-
     if (type === 'success') {
       // Build Firebase credential with the Facebook access token.
       const credential = firebase.auth.FacebookAuthProvider.credential(token);
-
       // Sign in with credential from the Facebook user.
       firebase.auth().signInAndRetrieveDataWithCredential(credential).catch((error) => {
         // Handle Errors here.
--- src/components/SocialLogin/Google.js
+++ src/components/SocialLogin/Google.js
@@ -2,20 +2,15 @@ import React from 'react'
 import { Button, View, Text } from 'react-native'
 import { Google } from 'expo';
 import * as firebase from 'firebase'
+import { GOOGLE_CLIENT_ID } from 'src/environement'
 
 class GoogleButton extends React.Component {
   onClickLogin = async () => {
-    const clientId = "88989378200-bul8u36qdlvjoo2h6j3csaq59qgjgaot.apps.googleusercontent.com"
-    // put this client id in the firebase console
-    // create client android https://console.cloud.google.com/apis/credentials?
-    // see https://blog.expo.io/google-sign-in-with-react-native-and-expo-9cac6c392f0e
     try {
-      const { type, idToken, ...rest } = await Google.logInAsync({ clientId });
-      console.log(type, idToken, rest)
+      const { type, idToken } = await Google.logInAsync({ GOOGLE_CLIENT_ID });
       if (type === 'success') {
         // Build Firebase credential with the Google access token.
         const credential = firebase.auth.GoogleAuthProvider.credential(idToken);
-
         // Sign in with credential from the Facebook user.
         firebase.auth().signInAndRetrieveDataWithCredential(credential).catch((error) => {
           // Handle Errors here.
